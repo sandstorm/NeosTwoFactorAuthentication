a0667d1e3ee4a8a5b6c70589473a668d052c272e
Robert Baruck <robert.baruck@sandstorm.de>
2024-09-25 10:32:54 +0200

    TASK: Revert SecondFactorDto refactoring

--- Classes/Domain/Model/Dto/SecondFactorDto.php
+++ Classes/Domain/Model/Dto/SecondFactorDto.php
@@ -5,12 +5,31 @@ namespace Sandstorm\NeosTwoFactorAuthentication\Domain\Model\Dto;
 use Neos\Neos\Domain\Model\User;
 use Sandstorm\NeosTwoFactorAuthentication\Domain\Model\SecondFactor;
 
-final class SecondFactorDto
+class SecondFactorDto
 {
-    public function __construct(
-        readonly public SecondFactor $secondFactor,
-        readonly public ?User        $user = null
-    )
+    protected SecondFactor $secondFactor;
+
+    protected User $user;
+
+    public function __construct(SecondFactor $secondFactor, User $user = null)
     {
+        $this->user = $user;
+        $this->secondFactor = $secondFactor;
+    }
+
+    /**
+     * @return SecondFactor|string
+     */
+    public function getSecondFactor(): SecondFactor
+    {
+        return $this->secondFactor;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
     }
 }
